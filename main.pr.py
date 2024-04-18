cd277ed7a78011f58e19012519521aa54b2ebab4
Dawidy574747 <166691720+Dawidy574747@users.noreply.github.com>
2024-04-18 09:27:12 +0200

    wip: add a menu

--- main.pr.py
+++ main.pr.py
@@ -11,4 +11,16 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twoj znajomy {user['name']} opublikowal: {user['posts']}")
 
-show_users(users)
+
+if __name__ == "__main__":
+    print("Witaj uzytkowniku")
+    while True:
+
+        print("Menu:")
+        print("1. Wyswietl co u znajomych")
+        menu_option: str = input("dokonaj wyboru:")
+        if menu_option == "0":
+            print("Program konczy prace")
+            break
+        if menu_option == "1":
+            show_users(users)
