3f2214f55e517369db5890a6c0edd1ca0b327a59
Dawidy574747 <166691720+Dawidy574747@users.noreply.github.com>
2024-04-18 08:44:16 +0200

    wip: Add user printing functionality

---
+++ main.pr.py
@@ -0,0 +1,18 @@
+imie="Dawid"
+nazwisko="Baluka"
+liczba_postow=6000
+
+print(f"twoj znajomy {imie} {nazwisko} opublikował {liczba_postow} postow")
+
+imie_2="Kevin"
+nazwisko_2="Czajkowski"
+liczba_postow_2=6002
+
+print(f"twoj znajomy {imie_2} {nazwisko_2} opublikował {liczba_postow_2} postow")
+
+imie_3="Kamil"
+nazwisko_3="Gil"
+liczba_postow_3=100000
+
+print(f"twoj znajomy {imie_3} {nazwisko_3} opublikował {liczba_postow_3} postow")
+
