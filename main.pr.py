469c012947400ff2c09b587eaf4a5d93bbbabf6f
Dawidy574747 <166691720+Dawidy574747@users.noreply.github.com>
2024-04-18 10:03:34 +0200

    wip: refactoring to folders

--- main.pr.py
+++ main.pr.py
@@ -1,15 +1,10 @@
-users: list[dict] = [
-    {"name": "Dawid", "sourname": "Baluka", "posts": 6000},
-    {"name": "Kevin", "sourname": "Czajkowski", "posts": 6002},
-    {"name": "Kamil", "sourname": "Gil", "posts": 1000000},
-    {"name": "Daniel", "sourname": "Blaszczyk", "posts": 6}
-
-]
+from models.data import users
+from utils.crud import show_users
 
 
-def show_users(user_list: list[dict]) -> None:
-    for user in user_list:
-        print(f"Twoj znajomy {user['name']} opublikowal: {user['posts']}")
+
+
+
 
 
 if __name__ == "__main__":
