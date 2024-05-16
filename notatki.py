7f309e18c96fa1f5275d05eb2d539c2eb8e127a8
Dawidy574747 <166691720+Dawidy574747@users.noreply.github.com>
2024-05-16 11:17:45 +0200

    wip: tk_gui

--- notatki.py
+++ notatki.py
@@ -1,8 +1,6 @@
-from models.data import users
-from untils.crud import show_users
+def __init__(self, name, username, posts, locations):
+    self.name = name
+    self.username = username
+    self.posts = posts
+    self.location = location
 
-
-
-
-search_user(users)
-print(users)
